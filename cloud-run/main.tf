eb63797f9f831a049372492db875cc3952c66801
Hatzz <git@hatzz.io>
2022-03-04 13:16:05 +0100

    Initial commit

---
+++ cloud-run/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.12.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+resource "google_cloud_run_service" "main" {
+  name     = var.app_name
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = var.container_image
+
+        ports {
+          name           = var.ports["name"]
+          container_port = var.ports["port"]
+        }
+
+        dynamic "env" {
+          for_each = var.env
+          content {
+            name  = env.value["name"]
+            value = env.value["value"]
+          }
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location = google_cloud_run_service.main.location
+  project  = google_cloud_run_service.main.project
+  service  = google_cloud_run_service.main.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
---
+++ cloud-run/variables.tf
@@ -0,0 +1,40 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "env" {
+  type = list(object({
+    value = string
+    name  = string
+  }))
+  description = "Environment variables (cleartext)"
+  default     = []
+}
+
+variable "ports" {
+  type = object({
+    name = string
+    port = number
+  })
+  description = "Port which the container listens to (http1 or h2c)"
+  default = {
+    name = "http1"
+    port = 8080
+  }
+}
+
+variable "container_image" {
+  type = string
+}
