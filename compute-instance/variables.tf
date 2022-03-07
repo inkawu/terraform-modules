445bf6ec8bc8b2b100e930ac0d84b8a9cfc2a17a
Hatzz <git@hatzz.io>
2022-03-07 21:37:37 +0100

    Fix variables

---
+++ compute-instance/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  type = string
+}
+
+variable "boot_disk_size" {
+  type = number
+}
+
+variable "machine_type" {
+  type = string
+}
+
+variable "boot_disk_type" {
+  type = string
+}
+
+variable "image" {
+  type = string
+}
